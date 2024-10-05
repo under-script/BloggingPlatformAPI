cf24e495a5d51c405714a87fdbc4b2c6b5a6de0b
uzinfocomga <uzinfocomga@gmail.com>
2024-10-05 16:04:25 +0500

    .

--- BloggingPlatformAPI/settings.py
+++ BloggingPlatformAPI/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
